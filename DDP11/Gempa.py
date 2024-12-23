bf946779d1b635cea0d3bf914374ca4f77ac463f
Asyiahh <asyiaahh123@gmail.com>
2024-12-23 13:05:28 +0700

    praktikum 11

---
+++ DDP11/Gempa.py
@@ -0,0 +1,24 @@
+class Gempa:
+    lokasi = ""
+    skala = ""
+
+    # contractor
+    def __init__(self, lokasi, skala):
+        self.lokasi = lokasi
+        self.skala = skala
+
+    # method/fungsi
+    def dampak(self):
+        if self.skala <2 :
+            ket = "Gempa tidak berasa"
+        elif self.skala >=2 and self.skala <4 :
+            ket = "Bangunan retak-retak"
+        elif self.skala >=4 and self.skala <6 :
+            ket = "Bangunan roboh"
+        else :
+            ket = "Bangunan roboh dan berpotensi tsunami"
+
+        print("Lokasi Gempa", self.lokasi,
+                  "\nSkala Gempa", self.skala,
+                  "\nDampak Gempa", ket)
+        print("_____"*5)
